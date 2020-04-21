65b55f660b0a342defa03e5a98f5ea96e0b9544e
k_yamanaka <vaivailx+git@gmail.com>
2020-04-21 23:36:58 +0000

    12と13間違えてコミットしていたので修正する

--- chapter2/2_12.sh
+++ chapter2/2_12.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# タブ1文字につきスペース1文字に置換せよ．確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．
-echo 'exe sed'
-sed -e 's/\t/ /g' popular-names.txt
-echo 'exe tr'
-cat popular-names.txt | tr '\t' ' '
-echo 'exe expand'
-expand -t 1 popular-names.txt
+#各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
+cut -f 1 popular-names.txt > col1.txt
+cut -f 2 popular-names.txt > col2.txt
+
+echo 'result col1'
+cat col1.txt
+echo 'result col2'
+cat col2.txt
